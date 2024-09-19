cac6e22ae90499e60e899875045d06a5e289a2cd
Jashuu <iqjs0124@gmail.com>
2024-09-19 19:53:52 -0400

    Update 0919_3

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -7,6 +7,7 @@ export default function Labs() {
   return (
     <div>
       <h1>Labs</h1>
+
       <TOC />
       <Routes>
         <Route path="/" element={<Navigate to="Lab1" />} />
@@ -14,6 +15,18 @@ export default function Labs() {
         <Route path="Lab2" element={<Lab2 />} />
         <Route path="Lab3" element={<Lab3 />} />
       </Routes>
+
+      <footer style={{
+        marginTop: "20px", 
+        padding: "10px", 
+        textAlign: "center", 
+        backgroundColor: "#f1f1f1",
+        fontSize: "14px",
+        color: "#555"
+      }}>
+        Created by <strong>Jiashu Qian</strong>
+      </footer>
+
     </div>
   );
 }
