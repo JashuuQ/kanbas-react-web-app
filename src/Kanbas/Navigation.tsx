052169d72d25a0e0760574bd8297f868fc90ab1f
Jiashu <iqjs0124@gmail.com>
2024-10-03 22:34:16 -0400

    a2 CSS and Bootstrap - v1

--- src/Kanbas/Navigation.tsx
+++ src/Kanbas/Navigation.tsx
@@ -1,13 +1,75 @@
+import { AiOutlineDashboard } from "react-icons/ai";
+import { IoCalendarOutline } from "react-icons/io5";
+import { LiaBookSolid, LiaCogSolid } from "react-icons/lia";
+import { FaInbox, FaRegCircleUser } from "react-icons/fa6";
 import { Link } from "react-router-dom";
+
 export default function KanbasNavigation() {
   return (
-    <div id="wd-kanbas-navigation">
-      <a href="https://www.northeastern.edu/" id="wd-neu-link" target="_blank">Northeastern</a><br/>
-      <Link to="/Kanbas/Account" id="wd-account-link">Account</Link><br/>
-      <Link to="/Kanbas/Dashboard" id="wd-dashboard-link">Dashboard</Link><br/>
-      <Link to="/Kanbas/Courses" id="wd-course-link">Courses</Link><br/>
-      <Link to="/Kanbas/Calendar" id="wd-calendar-link">Calendar</Link><br/>
-      <Link to="/Kanbas/Inbox" id="wd-inbox-link">Inbox</Link><br/>
-      <Link to="/Labs" id="wd-labs-link">Labs</Link><br/>
+    <div id="wd-kanbas-navigation" 
+         className="
+              list-group rounded-0 position-fixed 
+              bottom-0 top-0 d-none d-md-block bg-black z-2" 
+         style={{ width: 120 }}>
+
+      {/* NU Logo */}
+      <a id="wd-neu-link" target="_blank" 
+         href="https://www.northeastern.edu/" 
+         className="list-group-item bg-black border-0 text-center">
+        <img src="/images/NU.png" alt="NEU" width="75px" />
+      </a>
+      <br />
+
+      {/* Account */}
+      <Link to="/Kanbas/Account" 
+        className="list-group-item text-white bg-black text-center border-0">
+        <FaRegCircleUser className="fs-1 text-white" /><br />
+        Account
+      </Link>
+      <br />
+
+      {/* Dashboard */}
+      <Link to="/Kanbas/Dashboard" 
+        className="list-group-item text-center border-0 bg-white text-danger">
+        <AiOutlineDashboard className="fs-1 text-danger" /><br />
+        Dashboard
+      </Link>
+      <br />
+
+      {/* Courses */}
+      <Link to="/Kanbas/Courses" 
+        className="list-group-item text-white bg-black text-center border-0">
+        <LiaBookSolid className="fs-1 text-danger" /><br />
+        Courses
+      </Link>
+      <br />
+
+      {/* Calendar */}
+      <Link to="/Kanbas/Calendar" 
+        className="list-group-item text-white bg-black text-center border-0">
+        <LiaBookSolid className="fs-1 text-danger" /><br />
+        Calendar
+      </Link>
+      <br />
+
+      {/* Inbox */}
+      <Link to="/Kanbas/Inbox" 
+        className="list-group-item text-white bg-black text-center border-0">
+        <LiaBookSolid className="fs-1 text-danger" /><br />
+        Inbox
+      </Link>
+      <br />
+
+      {/* Labs */}
+      <Link to="/Labs" 
+        className="list-group-item text-white bg-black text-center border-0">
+        <LiaBookSolid className="fs-1 text-danger" /><br />
+        Labs
+      </Link>
+      <br />
+     
     </div>
-);}
+  );
+}
+
+
