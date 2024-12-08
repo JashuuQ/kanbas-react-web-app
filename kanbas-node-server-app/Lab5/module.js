28951c02348097670f6e8ccdd6014b9a0ab540b2
Jashuu <iqjs0124@gmail.com>
2024-12-08 13:09:25 -0500

    Remove kanbas-node-server-app from version control

--- kanbas-node-server-app/Lab5/module.js
+++
@@ -1,8 +0,0 @@
-const module = {
-    id: "CS5610",
-    name: "Web Development",
-    description: "Learn to build full-stack web applications",
-    course: "Computer Science",
-};
-
-export default module;
