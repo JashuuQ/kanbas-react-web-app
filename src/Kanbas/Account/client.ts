daa655582a1ca046b1b40b77cdc61df51056aea7
Jashuu <iqjs0124@gmail.com>
2024-12-08 15:45:47 -0500

    fix small bugs

--- src/Kanbas/Account/client.ts
+++ src/Kanbas/Account/client.ts
@@ -3,8 +3,14 @@ import { User, Course } from '../types';
 
 const axiosWithCredentials = axios.create({ withCredentials: true });
 
-export const REMOTE_SERVER = '';
+// Check the remote server
+export const REMOTE_SERVER =
+  process.env.NODE_ENV === 'production'
+    ? process.env.REACT_APP_REMOTE_SERVER
+    : '';
+
 export const USERS_API = `${REMOTE_SERVER}/api/users`;
+
 
 // basic operations
 export const signin = async (credentials: any): Promise<User> => {
@@ -22,11 +28,16 @@ export const signin = async (credentials: any): Promise<User> => {
   }
 };
 
+export const profile = async (): Promise<User | null> => {
+  const response = await axiosWithCredentials.post<User | null>(
+    `${USERS_API}/profile`
+  );
+  return response.data;
+};
+
 export const signup = async (user: any): Promise<User> => {
   const response = await axiosWithCredentials.post<User>(
-    `${USERS_API}/signup`,
-    user
-  );
+    `${USERS_API}/signup`, user);
   return response.data;
 };
 
@@ -39,21 +50,12 @@ export const signout = async () => {
 export const updateUser = async (user: User): Promise<User> => {
   try {
     const response = await axiosWithCredentials.put<User>(
-      `${USERS_API}/${user._id}`,
-      user
-    );
+      `${USERS_API}/${user._id}`, user);
     return response.data;
   } catch (error) {
     console.error('Update user error:', error);
     throw error;
   }
-};
-
-export const profile = async (): Promise<User | null> => {
-  const response = await axiosWithCredentials.post<User | null>(
-    `${USERS_API}/profile`
-  );
-  return response.data;
 };
 
 // Course
@@ -67,9 +69,7 @@ export const findMyCourses = async (): Promise<Course[]> => {
 export const createCourse = async (course: Course): Promise<Course> => {
   try {
     const { data } = await axiosWithCredentials.post<Course>(
-      `${USERS_API}/current/courses`,
-      course
-    );
+      `${USERS_API}/current/courses`, course);
     return data;
   } catch (error) {
     console.error('Create course error:', error);
